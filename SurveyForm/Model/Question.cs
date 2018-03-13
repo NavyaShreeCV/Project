97f0e86b6c0a8de0c495dc3e0ddc10b3d4dbe225
Navyashree Cv <Navyashree.cv@HMECD000771>
2018-03-13 12:11:29 +0530

    My first commit message

---
+++ SurveyForm/Model/Question.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Model
+{
+    //Question details
+    public class Question
+    {
+        public int QuestionID;
+        public int SectionID;
+        public string Description;
+        public int OptionGroupID;
+    }
+    //record answers of questions
+    public class Survey
+    {
+        public int SurveyID;
+        public string Answers;
+        public int QuestionID;
+        public int TrainingID;
+    }
+    //Training details
+    public class TrainingProgram
+    {
+        public int TrainingID;
+        public string TrainingName;
+        public string Trainer;
+        public DateTime Date;
+        public string Venue;
+        public string Attendees;
+
+    }
+    //user details
+    public class UserTable
+    {
+        public int UserId;
+        public string UserName;
+    }
+
+    public class Admin
+    {
+        public string UserName;
+        public string Password;
+    }
+
+
+
+}
+
---
+++ SurveyForm/SurveyForm/Controllers/SurveyController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Model;
+using DAL;
+
+namespace SurveyForm.Controllers
+{
+    public class SurveyController : ApiController
+    {
+        SurveyQuestion allquestions = new SurveyQuestion();
+
+        /// <summary>
+        /// to get all the users to populate training dropdown
+        /// </summary>
+        /// <returns>list of users,List<UserTable></returns>
+        [HttpGet]
+        public List<UserTable> GetAllUsers()
+        {
+            List<UserTable> lists = new List<UserTable>();
+            lists = SurveyQuestion.GetUsers();
+            return lists;
+        }
+
+        /// <summary>
+        /// fetching questions of particular section
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> list of questions,List<Question></returns>
+        [HttpGet]
+        public List<Question> GetQuestions(int id)
+        {
+            List<Question> questions = allquestions.GetQuestions(id);
+            return questions;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Survey> GetFeedback(int id)
+        {
+            List<Survey> values = SurveyQuestion.GetAnswers(id);
+            return values;
+        }
+
+        /// <summary>
+        /// fetches training details to display
+        /// </summary>
+        /// <returns>list of trainingprograms,List<TrainingProgram></returns>
+        [HttpGet]
+        public List<TrainingProgram> GetTraining()
+        {
+            List<TrainingProgram> training = allquestions.Training();
+            return training;
+        }
+
+        /// <summary>
+        /// to insert answers into survey table of a particular survey 
+        /// </summary>
+        /// <param name="surveys"></param>
+        [HttpPost]
+        public void PostSurvey([FromBody]Survey[] survey)
+        {
+            allquestions.Survey(survey);
+        }
+
+        /// <summary>
+        /// to insert training details into TrainingProgram table
+        /// </summary>
+        /// <param name="training"></param>
+        [HttpPost]
+        public void PostTraining(TrainingProgram train)
+        {
+            SurveyQuestion.InsertTraining(train);
+        }
+        [HttpDelete]
+        public void Deletetraining(int id)
+        {
+            SurveyQuestion.DeleteTraining(id);
+        }
+        [HttpPut]
+        public void Edittraining(TrainingProgram training)
+        {
+            SurveyQuestion.EditTraining(training);
+        }
+
+        [HttpGet]
+        public List<Admin> GetAdmin()
+        {
+          
+            List<Admin> user= SurveyQuestion.GetAdmin();
+            return user;
+        }
+    }
+}
+
